b610f0de9e1aa2867ea08bb8b905092130f1cf22
Travis Lawson <travislawclimb@gmail.com>
2020-11-11 13:29:41 -0800

    Controller, View, and Model created from USer Stories as well as logic for Index method and Create method in Superheros.cs

---
+++ Superhero_Creator/Controllers/Superheros.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SQLitePCL;
+using Superhero_Creator.Data;
+using Superhero_Creator.Models;
+
+namespace Superhero_Creator.Controllers
+{
+    public class Superheros : Controller
+    {
+        private ApplicationDbContext _context;
+        public Superheros(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: Superheros
+        public ActionResult Index()
+        {
+            var heroes = _context.Superheros.ToList();
+            return View(heroes);
+        }
+
+        // GET: Superheros/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Superheros/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Superheros/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                _context.Superheros.Add(superhero);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superheros/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Superheros/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superheros/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Superheros/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Superhero_Creator/Data/ApplicationDbContext.cs
+++ Superhero_Creator/Data/ApplicationDbContext.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Superhero_Creator.Models;
 
 namespace Superhero_Creator.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Superhero> Superheros { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            
         }
     }
 }
